4ddec21588b12b51879a5b5e954c80fdf4033997
asim16111980 <abomido2892016@gmail.com>
2024-12-18 15:13:21 +0200

    Complete TODO List App

--- main.js
+++ main.js
@@ -6,12 +6,17 @@ const todoList = document.querySelector(".to-do-list");
 // Retrieve and display data from LocalStorage
 const todoItems = localStorage.getItem("todo-items");
 // Create new Todo item function
-const createNewItem = () => {
+const createNewItem = (text) => {
   const newItemFragment = new DocumentFragment();
   const newItemContainer = document.createElement("div");
+  newItemContainer.dataset.id = 1;
+  newItemContainer.classList.add("to-do-item", "rounded");
   const readingMark = document.createElement("span");
+  readingMark.classList.add("reading-mark","rounded-full")
   newItemContainer.appendChild(readingMark);
   const itemText = document.createElement("span");
+  const itemTextNode = document.createTextNode(text);
+  itemText.append(itemTextNode);
   newItemContainer.appendChild(itemText);
   const itemEdit = document.createElement("button");
   newItemContainer.appendChild(itemEdit);
@@ -22,10 +27,10 @@ const createNewItem = () => {
 };
 // newItemBtn click event
 newItemBtn.addEventListener("click", () => {
-  let newItemText
-  if (newItemText.value !== "") {
-    console.log(typeof newItemText.value);
-    "m".trim()
-    createNewItem();
+  let trimmedText = newItemText.value.trim();
+  console.log(trimmedText);
+  if (trimmedText !== "") {
+    createNewItem(trimmedText);
+    newItemText.value = "";
   }
 });
